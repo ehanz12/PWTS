46c4eb8211722993cbf8539abbf6f9e526680152
ehanz12 <rhanss@gmail.com>
2025-01-22 23:17:46 +0700

    Finishing

--- TugasPertemuan1/component/js/script.js
+++ TugasPertemuan1/component/js/script.js
@@ -1,6 +1,11 @@
 const MenuIcon = document.getElementById("menu-icon")
 const MenuList = document.getElementById("menu-list")
+const Not = document.getElementById("not")
 
 MenuIcon.addEventListener("click", () => {
     MenuList.classList.toggle("hidden")
+})
+
+Not.addEventListener("click", ()=> {
+    alert("maaf project ini tidak bisa diakses")
 })
